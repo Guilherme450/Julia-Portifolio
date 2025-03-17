cb69fdd6dc107b04d512de49e8fc5c8a68451de8
Guilherme Lima <guilherme.l450@protonmail.com>
2025-03-17 13:35:21 -0300

    bug in addNewBook fixed

--- estudo_I.jl
+++ estudo_I.jl
@@ -33,32 +33,32 @@ function addBookData(arr_book::Vector{Book}, bookTitle::String, authorName::Stri
 end
 
 # criar métodos para inserção, remoção, pesquisa e atualização de dados
-function addNewBook(stock::Stock, arr_book::Vector{Book}, quantity::Int=1)
+function addNewBook(stock::Stock, book::Book, quantity::Int=1)
     if quantity < 0
         error("quantity must be greater or equal to zero")
     end
 
     # verificar se o livro já existe no inventário
-    for book in arr_book
-        if haskey(stock.books, book)
-            stock.books[book] += quantity
-        else
-            stock.books[book] = quantity
-        end
+    if haskey(stock.books, book)
+        stock.books[book] += quantity
+    else
+        stock.books[book] = quantity
     end
 end
 
 function main()
     addBookData(array_books, "O Senhor dos Anéis", "J.R.R. Tolkien", convert(Float16, 50.9),
     convert(Int16, 1200))
-    addBookData(array_books, "O Senhor dos Anéis", "J.R.R. Tolkien", convert(Float16, 50.9),
-    convert(Int16, 1200))
+
+    addBookData(array_books, "Naruto", "Masashi Kishimoto", convert(Float16, 30.9),
+    convert(Int16, 60))
 
     println(array_books[1])
 
     stock = Stock()
 
-    addNewBook(stock, array_books)
+    addNewBook(stock, array_books[1], 5)
+    addNewBook(stock, array_books[2], 20)
 
     println(stock.books)
 end
